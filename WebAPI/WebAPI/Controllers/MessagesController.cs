291a7cce030613f59db1521672d4674326cb24dc
Oriya Yehudai <oria3420@gmail.com>
2022-05-23 17:43:03 +0300

    Post Message API

--- WebAPI/WebAPI/Controllers/MessagesController.cs
+++ WebAPI/WebAPI/Controllers/MessagesController.cs
@@ -13,6 +13,26 @@ namespace WebAPI.Controllers
     {
         private IDataService<User> userService;
         private IDataService<Talk> talkService;
+
+        private Talk getTalk(string user, string id)
+        {
+            var userObject = userService.Get(user);
+            if (userObject == null) return null;
+
+            var contact = userObject.Contacts.Find(c => c.Id == id);
+            if (contact == null) return null;
+
+            List<string> talkers;
+            foreach (var talk in talkService.GetAll())
+            {
+                talkers = talk.Talkers.ToList();
+                if (talkers.Contains(userObject.Id) && talkers.Contains(contact.Id))
+                {
+                    return talk;
+                }
+            }
+            return null;
+        }
         public MessagesController()
         {
             userService = new UserDataService();
@@ -24,22 +44,26 @@ namespace WebAPI.Controllers
         [HttpGet]
         public IActionResult Get(string user, string id)
         {
-            var userObject = userService.Get(user);
-            if (userObject == null) return NotFound();
+            Talk talk = getTalk(user, id);
+            if(talk== null) return NotFound();
 
-            var contact = userObject.Contacts.Find(c => c.Id == id);
-            if (contact == null) return NotFound();
+            return Ok(talk.MessagesList);
+        }
 
-            List<string> talkers;
-            foreach (var talk in talkService.GetAll())
-            {
-                talkers = talk.Talkers.ToList();
-                if (talkers.Contains(userObject.Id) && talkers.Contains(contact.Id))
-                {
-                    return Ok(talk.MessagesList);
-                }
-            }
-            return NotFound();
+        // POST api/<ContactsController>/id/messages
+        [Route("/api/contacts/{id}/messages")]
+        [HttpPost]
+        public IActionResult Post(string id, [FromBody] PostMessageRequest request)
+        {
+            Talk talk = getTalk(request.User, id);
+            if (talk == null) return NotFound();
+
+            int msgId = talk.MessagesList[talk.MessagesList.Count - 1].Id;
+            Message message = new Message(msgId+1, request.Content, DateTime.Now,true, request.User);
+
+            talk.MessagesList.Add(message);
+
+            return StatusCode(201);
         }
     }
 }
---
+++ WebAPI/WebAPI/Models/Requests/PostMessageRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace WebAPI.Models.Requests
+{
+    public class PostMessageRequest
+    {
+        public string Content { get; set; }
+        public string User { get; set; }
+    }
+}
