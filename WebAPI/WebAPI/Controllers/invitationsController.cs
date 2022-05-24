8ae77666b065a87d0880ca32afa7c1162cbad88b
Oriya Yehudai <oria3420@gmail.com>
2022-05-24 14:50:35 +0300

    PostInvitationRequest

---
+++ WebAPI/WebAPI/Controllers/invitationsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+using WebAPI.Models.Requests;
+using WebAPI.Services;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class invitationsController : Controller
+    {
+        private IDataService<User> userService;
+
+        public invitationsController()
+        {
+            userService = new UserDataService();
+        }
+
+        // POST: api/<invitationsController>
+        [Route("/api/invitations")]
+        [HttpPost]
+
+        public IActionResult Post([FromBody] PostInvitationRequest request)
+        {
+            var userObjectFrom = userService.Get(request.From);
+            if (userObjectFrom == null) return NotFound();
+
+            var contactTo = userObjectFrom.Contacts.Find(c => c.Id == request.To);
+            if (contactTo == null) return NotFound();
+            userObjectFrom.AddContact(contactTo);
+        }
+
+}
+}
---
+++ WebAPI/WebAPI/Models/Requests/PostInvitationRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAPI.Models.Requests
+{
+    public class PostInvitationRequest
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Server { get; set; }
+
+    }
+}
